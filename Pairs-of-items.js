b34dc05f273859da54119555a8ebdacb8de567b5
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-29 20:40:32 -0800

    Update Pairs-of-items.js

--- Pairs-of-items.js
+++ Pairs-of-items.js
@@ -38,3 +38,16 @@ function numberOfPairs(gloves) {
 }
 
 // or
+
+function numberOfPairs(gloves) {
+  let counter = 0;
+  let copyGloves = [...gloves].sort();
+  for (let i = 0; i < copyGloves.length; i++) {
+      if (copyGloves[i] === copyGloves[i+1]) {
+          counter++;
+          copyGloves.splice(i, 2)
+          i--;
+      }
+  }
+  return counter;
+}
